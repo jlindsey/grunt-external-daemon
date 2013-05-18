85a5944a9b508a0f9db0de25de2327cc4eeefba6
Josh Lindsey <josh@core-apps.com>
2013-05-18 19:08:45 -0400

    Add test expectations to error test

--- test/external_daemon_errors_test.js
+++ test/external_daemon_errors_test.js
@@ -12,6 +12,8 @@ var grunt = require('grunt'),
     exec  = require('child_process').exec;
 
 exports.missing_cmd = function(test) {
+  test.expect(1);
+
   exec('grunt external_daemon:server_4', function(err, stdout, stderr) {
     var check = stdout.indexOf('Warning: You must specify "cmd" for task server_4');
     test.ok((check > -1), 'correct error');
@@ -20,6 +22,8 @@ exports.missing_cmd = function(test) {
 };
 
 exports.wrong_args = function(test) {
+  test.expect(1);
+
   exec('grunt external_daemon:server_4a', function(err, stdout, stderr) {
     var check = stdout.indexOf('Warning: You must specify "args" as an array for task server_4a');
     test.ok((check > -1), 'correct error');
@@ -28,6 +32,8 @@ exports.wrong_args = function(test) {
 };
 
 exports.bad_start_check = function(test) {
+  test.expect(1);
+  
   exec('grunt external_daemon:server_4b', function(err, stdout, stderr) {
     var check = stdout.indexOf('Warning: You must specify "startCheck" as a function for task server_4b');
     test.ok((check > -1), 'correct error');
